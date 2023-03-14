5158465cf23c23b8b2e4ff3ef9d85e67e017fff2
Mauricio Gabriel <souzamauricio91@hotmail.com>
2023-03-14 09:03:29 -0300

    Obtendo um usuario por id

--- src/main/java/com/mauricio/springmongodb/services/UserService.java
+++ src/main/java/com/mauricio/springmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.mauricio.springmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mauricio.springmongodb.domain.User;
 import com.mauricio.springmongodb.repository.UserRepository;
+import com.mauricio.springmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,11 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
 }
